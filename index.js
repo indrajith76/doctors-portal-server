c39500a2a3ed5edcd1a881d9d5a3c34ae591b695
indrajith76 <indrajithgoswami@gmail.com>
2022-11-18 16:31:27 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -9,15 +10,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w2fg5h5.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.w2fg5h5.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorsPortal")
+      .collection("appointmentOptions");
 
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Doctors Portal Server.");
