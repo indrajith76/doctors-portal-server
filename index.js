a0cc55fdb0248fcc41820d4a54f733500d2226e9
indrajith76 <indrajithgoswami@gmail.com>
2022-11-18 20:41:57 +0600

    implemeted functionality to find booked date and time

--- index.js
+++ index.js
@@ -19,16 +19,22 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    const appointmentOptionCollection = client
-      .db("doctorsPortal")
-      .collection("appointmentOptions");
-    const bookingsCollection = client
-      .db("doctorsPortal")
-      .collection("bookings");
+    const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+    const bookingsCollection = client.db("doctorsPortal").collection("bookings");
 
+    // Use Aggregate to query multiple collection and then marge data
     app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+    // code carefully :D
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
+        const bookedSlots = optionBooked.map(book=>book.slot)
+        console.log(date, option.name,bookedSlots)
+      });
       res.send(options);
     });
 
