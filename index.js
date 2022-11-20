21100b10d5587e19c10f9d1c6cf681ec36061e0a
indrajith76 <indrajithgoswami@gmail.com>
2022-11-20 15:37:23 +0600

    create api for Save Registered user information in the database

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
     const bookingsCollection = client
       .db("doctorsPortal")
       .collection("bookings");
+    const usersCollection = client.db("doctorsPortal").collection("users");
 
     // Use Aggregate to query multiple collection and then marge data
     app.get("/appointmentOptions", async (req, res) => {
@@ -109,6 +110,13 @@ async function run() {
      * app.delete('/bookings/:id')
      */
 
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const query = {
@@ -128,6 +136,13 @@ async function run() {
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
   } finally {
   }
 }
